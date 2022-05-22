545730de91560a69b923d9b8e4ca9bf8a7c41516
Saltuk Osmanlioglu <saltuk.osmanlioglu@gmail.com>
2022-05-22 16:50:43 +0200

    fix

--- src/pages/detail/video/tabs/Background.tsx
+++ src/pages/detail/video/tabs/Background.tsx
@@ -1,13 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
+
 import Accordion from "widgets/accordion";
 
 import "./Background.scss";
 
 const Background: React.FunctionComponent = () => {
+  const fileUpload = useRef<any>();
+
   return (
     <div className="background">
       <Accordion title="Images">
-        <div>image</div>
+        <div className="background-images">
+          <button onClick={() => fileUpload.current.click()}>
+            <img width={"100%"} src="/images/upload.png" alt="Background" />
+            <p>Upload</p>
+          </button>
+          <input ref={fileUpload} type="file" />
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background1.png"
+              alt="Background"
+            />
+            <p>Office</p>
+          </button>
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background2.png"
+              alt="Background"
+            />
+            <p>Space</p>
+          </button>
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background3.png"
+              alt="Background"
+            />
+            <p>Noise</p>
+          </button>
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background4.png"
+              alt="Background"
+            />
+            <p>Meeting Room</p>
+          </button>
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background5.png"
+              alt="Background"
+            />
+            <p>Books</p>
+          </button>
+          <button>
+            <img
+              width={"100%"}
+              src="/images/background6.png"
+              alt="Background"
+            />
+            <p>Desk</p>
+          </button>
+        </div>
       </Accordion>
       <Accordion title="Solid Colours">
         <div></div>
